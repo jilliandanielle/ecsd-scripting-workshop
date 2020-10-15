84dc3054a64a6ca951ad61b6165b6efcc96dd2d5
jillian <jilliandanielle@gmail.com>
2020-10-15 12:17:44 +0100

    first commit?

---
+++ task2/task2.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+Test=73
+Mod$(( $Test % 8 ))
+echo $Mod
+
+
+x=$(( x=4**6 ))
+y=$(( y=5**3 ))
+z=$(( x+y ))
+echo $z
+
+if [[ $Mod == $ x ]]; Then
+	echo "The numbers are the same."
+else 
+	echo "The numbers are not the same"
+fi
+
+ 
---
+++ task3/task3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if $3<4; then
+    echo $3" is less than 4"
+fi
